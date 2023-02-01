1479df33a02e7850c506cb2a89f99ffd7c953ca6
Pietro Fecarotta <pietrofecarotta2001@gmail.com>
2023-02-01 13:44:49 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <HelloWorld />
+  </div>
+</template>
+
+<style lang="scss">
+@import "./style/global.scss";
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "HelloWord",
+};
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  @include media-breakpoint-up(md) {
+    color: green;
+  }
+}
+</style>
