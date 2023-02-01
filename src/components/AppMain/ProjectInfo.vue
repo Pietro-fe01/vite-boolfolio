28fa89ab965dea7d06f7f6b9673a78f7474dc9f2
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-02-01 16:25:08 +0100

    Creato style per le immagini dell'API

--- src/components/AppMain/ProjectInfo.vue
+++ src/components/AppMain/ProjectInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card">
-        <img src="" class="card-img-top" alt="...">
+        <img v-if="projectData.image_url" :src="projectData.image_url" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ projectData.project_title }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{{ projectData.customer_name }}</h6>
@@ -31,4 +31,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .card {
+        & img {
+            height: 250px;
+            padding: 5px 5px 0 5px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            object-fit: cover;
+        }
+    }
 </style>
