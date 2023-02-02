77474e22227cb78ef4b4288297bcfae27e218b48
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-02-02 18:51:56 +0100

    Aggiunto style per la pagina di cortesia errors 404

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="error-404 container">
-        <h1 class="m-0">Page not found</h1>
+    <section class="error-404 container d-flex justify-content-center align-items-center">
+        <h4 class="m-0">Error 404 | Page not found</h4>
     </section>
 </template>
 
@@ -10,4 +10,13 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    section {
+        height: calc(100vh - 120px);
+        & h4 {
+            border: 3px solid black;
+            padding: 10px 15px;
+            border-radius: 10px;
+        }
+    }
+</style>
